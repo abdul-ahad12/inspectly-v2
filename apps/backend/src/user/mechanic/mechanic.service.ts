9b3f6465bde980ea1a67ec8265a94d5baef8602e
Shaik Noorullah <shaiknooru247@gmail.com>
2024-06-03 06:46:48 +0530

    fix((mechanic)): fix mechanic notifications not emitting. the code was commented

--- apps/backend/src/user/mechanic/mechanic.service.ts
+++ apps/backend/src/user/mechanic/mechanic.service.ts
@@ -10,6 +10,7 @@ import { SocketGateway } from '@/gateways/socket.gateway'
 import { PrismaService } from '@/prisma/prisma.service'
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import { ApprovalRequest, Mechanic, Prisma, User } from '@prisma/client'
+import { map } from 'lodash'
 // import { map } from 'lodash'
 import { z } from 'zod'
 
@@ -275,14 +276,14 @@ export class MechanicService {
 
     // const newMechs = selectTopMechanics(mechanics, 5)
 
-    // const booking = await this.bookingService.getBookingByBookingId(
-    //   findMechParams.bookingId,
-    // )
+    const booking = await this.bookingService.getBookingByBookingId(
+      findMechParams.bookingId,
+    )
 
     // // send booking request notifications to all mechanics
-    // map(newMechs, (mechanic) =>
-    //   this.notificationService.notifyMechanics(mechanic.id, booking),
-    // )
+    map(mechanics, (mechanic) =>
+      this.notificationService.notifyMechanics(mechanic.id, booking),
+    )
 
     return mechanics
   }
